f7b8702b5a9f8b8f0dc8b95838075defda933adc
Jesse Villines <43501112+jesservillines@users.noreply.github.com>
2025-04-19 09:31:07 -0600

    Prepare application for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  // This setting ensures compatibility with Netlify's serverless functions
+  target: process.env.NETLIFY ? 'serverless' : undefined,
 }
 
 module.exports = nextConfig
